83f16d3cede2f737a33e9e069749f8cbe0a825cc
Shymi613 <laura.sofia27021@gmail.com>
2025-03-19 15:46:48 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 
-def connect_to_mongodb(db_name, collection_name):
+def connect_to_mongodb(hospital, pacientes):
     uri = "mongodb+srv://laurasofia27021:Nochebuena1_@cluster0.9fw4c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[hospital]
